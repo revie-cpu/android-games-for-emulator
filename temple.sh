6701a2c37cd4b4e2ab3b2802d683ce25dc0697b5
revie-cpu <5takla510380@gmail.com>
2025-08-15 14:54:15 +0600

    Update temple.sh

--- temple.sh
+++ temple.sh
@@ -1,6 +1,9 @@
  wget --referer="https://www.apkmirror.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=344792&key=af12b2d9f411f474ab910c2eb8bb3c22cd9f8715" -O temple.apk
- echo "hill climb racing apk download done"
+clear
+ echo "temple run apk download done"
  docker cp temple.apk codespaces-blank-android-1:/tmp/temple.apk
+ clear 
+ echo "copy done"
  docker exec -it codespaces-blank-android-1 bash
 cat /proc/cpuinfo | grep vmx
 adb install /tmp/temple.apk
