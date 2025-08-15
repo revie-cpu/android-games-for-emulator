8486250a8eb4fa0833690c900fda127b8a7d9646
revie-cpu <5takla510380@gmail.com>
2025-08-15 14:52:15 +0600

    Update fruit-ninja.sh

--- fruit-ninja.sh
+++ fruit-ninja.sh
@@ -1,6 +1,6 @@
  wget --referer="https://www.apkmirror.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=8912645&key=29d34cff01970a3b798eeb2af32f4f749648b60e" -O fruit-ninja.apk
  clear
- echo "hill climb racing apk download done"
+ echo "fruit apk download done"
  docker cp fruit-ninja.apk codespaces-blank-android-1:/tmp/fruit-ninja.apk
  clear 
  echo "copy done"
