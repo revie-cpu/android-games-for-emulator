e6e2aae8b6fdbf2b259ca5a279685763108677c2
revie-cpu <5takla510380@gmail.com>
2025-08-16 13:18:27 +0000

    ahhhhhhhh

--- hill-climb.sh
+++ hill-climb.sh
@@ -1,4 +1,4 @@
- wget --referer="https://www.apkmirror.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=9331287&key=835341c472d6700177de303d4216e2d28761c552" -O hill.apk
+ wget --referer="https://www.rawgithubusercontent.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "https://raw.githubusercontent.com/revie-cpu/android-games-for-emulator/main/apps/hill.apk" -O hill.apk
  echo "hill climb racing apk download done"
  docker cp hill.apk codespaces-blank-android-1:/tmp/hill.apk
  docker exec -it codespaces-blank-android-1 bash
