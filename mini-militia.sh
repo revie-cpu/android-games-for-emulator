28bb662b5a5d6c03d139267dbb50c736d6c5defa
revie-cpu <5takla510380@gmail.com>
2025-08-16 12:41:46 +0600

    Update mini-militia.sh

--- mini-militia.sh
+++ mini-militia.sh
@@ -1,4 +1,4 @@
- wget --referer="https://www.apkmirror.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "" -O mini-militia.apk
+ wget --referer="https://www.apkmirror.com/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36" "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=347342&key=1e4c00e5ac28021c17e84a819d11546ac2ba0fca" -O mini-militia.apk
  clear
  echo "mini militia apk download done"
  docker cp mini-militia.apk codespaces-blank-android-1:/tmp/mini-militia.apk
